ee4459115c85d3790debf177914315042bbec15e
aklump <sourcecode@intheloftstudios.com>
2015-06-21 12:31:18 -0700

    Version bumped from 1.3.6 to 1.3.7

--- .web_package/hooks/build/1_jquery_plugin.php
+++ .web_package/hooks/build/1_jquery_plugin.php
@@ -23,7 +23,7 @@
  * Date: {{ date }}
  */
 
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
+require_once dirname(__FILE__) . '/../../vendor/autoload.php';
 
 // Translate arguments to vars.
 list(
